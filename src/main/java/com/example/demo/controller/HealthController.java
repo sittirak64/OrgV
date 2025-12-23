8030af54070f952ab24d1896fb5df563caf83975
Jekjirasin <jirasintankul2545@gmail.com>
2025-12-23 12:35:33 +0700

    เพิ่มไฟล์  HC

---
+++ src/main/java/com/example/demo/controller/HealthController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Backend is running";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
